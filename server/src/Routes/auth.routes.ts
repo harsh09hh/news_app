3158770810f39d3b2dd62ff6f0c036d8366dfc42
Harsh <harshvsingh137@gmail.com>
2025-12-30 12:01:49 +0530

    chged the rout for protected rout

--- server/src/Routes/auth.routes.ts
+++ server/src/Routes/auth.routes.ts
@@ -14,8 +14,6 @@ authRouter.post('/sign-in',signin);
 authRouter.post('/refresh',refreshAccessToken);
 
 authRouter.post('/sign-out',signout);
-authRouter.get('/me',verifyAccessToken);
-// authRouter.post('/forgot-password',);
 
 
 export default authRouter;
